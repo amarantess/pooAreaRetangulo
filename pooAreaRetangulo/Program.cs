be57cf01919ef9877bb19592aee8199ea492148e
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-04-15 11:11:53 -0300

    Add project files.

---
+++ pooAreaRetangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooAreaRetangulo
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("CALCULA A AREA DE UM RETANGULO");
+
+            Console.Write("Informe a altura: ");
+            retangulo.Altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a base: ");
+            retangulo.Base = double.Parse(Console.ReadLine());
+
+            retangulo.ExibeDados();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pooAreaRetangulo/Retangulo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooAreaRetangulo
+{
+    public class Retangulo
+    {
+        // Construtor
+        public Retangulo()
+        {
+            this.Altura = 0;
+            this.Base = 0;
+        }
+         
+        // Propriedades
+        // Base do retângulo
+        private double baseret;
+
+        public double Base
+        {
+            get { return baseret; }
+            set 
+            {
+                if (value >= 0) baseret = value;
+                else baseret = 0;
+            }
+        }
+
+        // Altura do retângulo
+        private double altret;
+
+        public double Altura
+        {
+            get { return altret; }
+            set 
+            { 
+                if (value >= 0) altret = value; 
+                else altret = 0;
+            }
+        }
+
+        // Area do retângulo
+        public double Area
+        {
+            get { return altret * baseret; }
+        }
+
+        // Exibir dados
+        public void ExibeDados()
+        {
+            Console.WriteLine($"Base: {this.Base}");
+            Console.WriteLine($"Altura: {this.Altura}");
+            Console.WriteLine($"Area: {this.Area}");
+        }
+
+    }
+}
